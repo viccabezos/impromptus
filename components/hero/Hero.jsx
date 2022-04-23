b11c68d5da5517821a126e278ed8f53996a8b951
Victoria Cabezos <victoria.cabezos@origins-digital.com>
2022-04-23 11:06:51 +0200

    hero comp and dynamic chanter page

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,20 +1,22 @@
 import React from "react";
-
+import Image from "next/image";
+import Hoss from "../../public/hoss.webp";
 function Hero() {
   return (
     <div
       data-theme="mytheme"
-      className="hero min-h-screen bg-primary-focus"
+      className="hero min-h-screen bg-white"
       //   style="background-image: url(https://api.lorem.space/image/fashion?w=1000&h=800);"
     >
+      <Image src={Hoss} layout="fill" className="opacity-10" />
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Les chanteurs impromptus</h1>
+          <p className="mb-2">Bienvenue à tous chanteurs de tout horizon !</p>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            "Le chant est un langage universel qui doit rassembler les femmes et
+            les hommes de tous pays et de toutes cultures.""
           </p>
           <button className="btn btn-primary text-white">
             <a href="/chanter">CHANTER</a>
