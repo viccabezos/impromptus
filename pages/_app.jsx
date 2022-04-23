84486a51ad0080b6712676463e223d4b9eee7441
Victoria Cabezos <victoria.cabezos@origins-digital.com>
2022-04-23 10:31:32 +0200

    social links navbar and footer styling according to template

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,10 @@
 import Footer from "../layout/footer/Footer";
 import Navbar from "../layout/navbar/Navbar";
-import NavBar2 from "../layout/navbar/NavBar2";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      {/* <NavBar2 /> */}
       <Navbar />
       <Component {...pageProps} />
       <Footer />
